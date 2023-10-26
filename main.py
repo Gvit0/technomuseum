de1e6b201075bc61c7d522df13fadf668ed215e4
Gvit0 <gatvit@mail.ru>
2023-10-26 20:58:39 +0300

    add links ar ar-online museum museumvov and create menuup

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/logreg')
+def logreg():
+    return render_template('logreg.html')
 
+@app.route('/museum')
+def museum():
+    return render_template('museum.html')
 
+@app.route('/museumvov')
+def museumvov():
+    return render_template('museumvov.html')
 
+@app.route('/ar')
+def ar():
+    return render_template('ar.html')
+
+@app.route('/ar-online')
+def aronline():
+    return render_template('ar-online.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
